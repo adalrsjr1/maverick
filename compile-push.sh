841b9e83161ff4cfb30dec799e8a8b6003b851bb
adalrsjr1 <adalrsjr1@gmail.com>
2017-06-04 16:19:15 -0700

    deployment scripts

--- compile-push.sh
+++ compile-push.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-mvn clean package -DpushImage
+#mvn clean package -DpushImage
+docker run -d -p 5672:5672 --name rabbitmq rabbitmq ; docker run -d -p 27017:27017 --name mongo mongo ; mvn clean package

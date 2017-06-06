2ebcb21d6f2f7efd8c29be8a1df78848292a29d2
Adalberto Junior <adalrsjr1@gmail.com>
2017-06-05 17:11:26 -0700

    added fluentd images on deployment

--- push-images.sh
+++ push-images.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+echo "building Fluentd"
+echo ""
+
+docker build --no-cache --rm -t adalrsjr1/fluentd -f deployment/fluentd-vanilla/Dockerfile ./deployment/fluentd-vanilla
+
+echo "building Fluentd-ELK"
+echo ""
+docker build --no-cache --rm -t adalrsjr1/fluentd-efk -f deployment/fluentd-efk/Dockerfile ./deployment/fluentd-efk
+
+
 for IMG in $(docker images | grep adalrsjr1 | awk '{print $1}')
 do
   echo ">>> $IMG pushing to Docker HUB"
+  echo ""
   docker push $IMG
 done
+
+
